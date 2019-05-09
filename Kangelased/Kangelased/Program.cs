4654ca853042e037a46c6de864de40821ae0c9ac
opilane <opilane@HP-CZC6297B97>
2019-05-09 12:28:40 +0300

    3 ülesanne Faililugemise meetod

--- Kangelased/Kangelased/Program.cs
+++ Kangelased/Kangelased/Program.cs
@@ -8,6 +8,29 @@ namespace Kangelased
 {
 	class Program
 	{
+        static List<Kangelane> Kangelased= new List<Kangelane>();
+        static void LaeKangelased(string Failinimi)
+        {
+            string[] failiandmed = System.IO.File.ReadAllLines(Failinimi);
+            for (int i = 0; i < failiandmed.Length; i++)
+            {
+                string[] nimikoht = failiandmed[i].Split(new char[] { '/' });
+                string nimi = nimikoht[0].Trim();
+                string asukoht = nimikoht[1].Trim();
+                if (nimi.EndsWith("*"))
+                {
+                    Kangelased.Add(new Superkangelane(nimi.Trim(new char[] { '*' }), asukoht));
+
+                }
+                else
+                {
+                    Kangelased.Add(new Kangelane(nimi, asukoht));
+
+                }
+
+            }
+
+        }
 		static void Main(string[] args)
 		{
 
