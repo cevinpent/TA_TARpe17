b25dddb198dd97d4ceef11727ca889fde4eb94a9
opilane <opilane@HP-CZC6297B93>
2019-05-16 14:07:23 +0300

    esimene punkt Kangelased kordamine failis

--- Kangelased/Kangelased/Kangelane.cs
+++ Kangelased/Kangelased/Kangelane.cs
@@ -12,15 +12,33 @@ namespace Kangelased
 
         private string asukoht;
 
+		private string superNimi;
+
+		private string v6ime;
+
+
+
         protected string Nimi { get => nimi; set => nimi = value; }
         protected string Asukoht { get => asukoht; set => asukoht = value; }
+		public string SuperNimi { get => superNimi; set => superNimi = value; }
+		public string V6ime { get => v6ime; set => v6ime = value; }
 
-        public Kangelane(string Nimi, string Asukoht)
+		public Kangelane(string Nimi, string Asukoht)
         {
             this.Nimi = Nimi;
             this.Asukoht = Asukoht;
         }
-        public int Päästa(int Ohustatuid)
+
+		public Kangelane(string Nimi, string SuperNimi, string V6ime)
+		{
+
+			this.Nimi = Nimi;
+			this.SuperNimi = SuperNimi;
+			this.V6ime = V6ime;
+
+		}
+
+		public int Päästa(int Ohustatuid)
         {
             return (int)Math.Round(Ohustatuid * .95);
         }
